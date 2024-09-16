2ac8f81403c16907cda2e84b8bc9b00f27016ef6
winiyaz <winiyaz@proton.me>
2024-09-16 13:41:46 +0000

    .

--- w1/m1.py
+++ w1/m1.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python3
 
+## Prettification code 
+# PRettifer 
+# Prettifyer code
+
+# ------------------------------------------------------
+from rich import print as rprint # For rprinting
+from rich.pretty import pprint # For pretty printing
+from rich import inspect # For inspect
+from rich.console import Console # For console.print
+from rich.markdown import Markdown # For markdown
+from rich.panel import Panel # For Panel()
+from rich import box # For Panel Boxes
+from rich.prompt import Prompt # For Prompting 
+from rich.style import Style # For styles colors
+from rich.text import Text # For text Styles
+console = Console() # Standard code to access console
+from rich.traceback import install
+install(show_locals=True)
+# -------------------------------------------------------
+##
+
 import requests as rq
 
-url = ""
+url = "https://api.open-notify.org/iss-now.json"
 rq.get(url).json()
 
